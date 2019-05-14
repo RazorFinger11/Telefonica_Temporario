29e3b0a85c3446480cdf27be0a15099d17da4fd2
RazorFinger <pedro.lopes0203@gmail.com>
2019-05-14 17:49:46 -0300

    FIRST COMMIT

---
+++ Challenge_Game/Assets/AnswerManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[System.Serializable]
+public class Answers
+{
+    // Answers written down in your buttons
+    public string[] answerText;
+
+    // Value of each option
+    public int[] values;
+
+    // The reaction of the client after you click a button
+    public Reactions[] reactionSentences;
+
+    [HideInInspector]
+    public int index;
+}
+
+[System.Serializable]
+public class Reactions
+{
+    public Sentences[] sentences;
+
+    [HideInInspector]
+    public int index;
+}
+
+public class AnswerManager : MonoBehaviour
+{
+    public Button[] answerButtons;
+
+    public Answers[] answerArray;
+
+    [HideInInspector]
+    public int answerNumber;
+
+    public ClientSentences clientScript;
+    public PointManager pointScript;
+
+    // weird ass button logic
+    public void OnEnable()
+    {
+        // when clicked it will call the "CallClientReaction" function, and pass on the appropriate reaction and add the appropriate amount of points
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            int closureIndex = i;
+            answerButtons[closureIndex].onClick.AddListener(() => callClientReaction(answerArray[answerNumber].reactionSentences[closureIndex].sentences));
+            answerButtons[closureIndex].onClick.AddListener(() => addPoints(answerArray[answerNumber].values[closureIndex]));
+        }
+    }
+
+    // call the client writing script to make them write a reaction
+    public void callClientReaction(Sentences[] reaction)
+    {
+        clientScript.reactionIndex = 0;
+        clientScript.StartCoroutine(clientScript.typeReaction(reaction));
+    }
+
+    public void OnDisable()
+    {
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            int closureIndex = i;
+            answerButtons[closureIndex].onClick.RemoveAllListeners();
+        }
+    }
+
+    // Add Points
+    public void addPoints(int value)
+    {
+        pointScript.AddPoints(value);
+    }
+
+    // Display Buttons
+    public void DisplayAnswers()
+    {
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            // Enable the buttons
+            answerButtons[i].gameObject.SetActive(true);
+            // Attribute your text to them
+            answerButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = answerArray[answerNumber].answerText[i];
+        }
+
+    }
+
+    // Hide Buttons
+    public void DisableAnswers()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            // Disable the buttons
+            answerButtons[i].gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Challenge_Game/Assets/CameraShaker.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CameraShaker : MonoBehaviour
+{
+    [System.Serializable]
+    public class Properties
+    {
+        public float angle;
+        public float strength;
+        public float speed;
+        public float duration;
+
+        [Range(0, 1)]
+        public float noisePercent;
+        [Range(0, 1)]
+        public float dampingPercent;
+        [Range(0, 1)]
+        public float rotationPercent;
+
+    }
+
+    public Properties testProperties;
+    IEnumerator curShakeCoroutine;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartShake(testProperties);
+        }
+    }
+
+    public void StartShake(Properties properties)
+    {
+        if (curShakeCoroutine != null)
+        {
+            StopCoroutine(curShakeCoroutine);
+        }
+
+        curShakeCoroutine = Shake(properties);
+        StartCoroutine(Shake(properties));
+    }
+
+    IEnumerator Shake(Properties properties)
+    {
+        float completionPercent = 0;
+        float movePercent = 0;
+
+        float angle_radians = properties.angle * Mathf.Deg2Rad - Mathf.PI;
+        Vector3 previousWaypoint = new Vector3(0, 0, transform.position.z);
+        Vector3 curWaypoint = new Vector3(0, 0, transform.position.z);
+        float moveDistance = 0;
+
+        while(true)
+        {
+            if (movePercent >= 1 || completionPercent == 0)
+            {
+                float noiseAngle = (Random.value - .5f) * Mathf.PI;
+                angle_radians += Mathf.PI + noiseAngle * properties.noisePercent;
+
+                curWaypoint = new Vector3(Mathf.Cos(angle_radians), Mathf.Sin(angle_radians)) * properties.strength;
+                previousWaypoint = transform.localPosition;
+
+                moveDistance = Vector3.Distance(curWaypoint, previousWaypoint);
+                movePercent = 0;
+            }
+
+            completionPercent += Time.deltaTime/properties.duration;
+            movePercent += Time.deltaTime / moveDistance * properties.speed;
+            transform.localPosition = Vector3.Lerp(previousWaypoint, curWaypoint, movePercent);
+
+            yield return null;
+        }
+    }
+}
