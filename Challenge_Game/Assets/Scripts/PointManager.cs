48ff35c84c85af3d317af24c7ab97a967281b246
devlog98 <dno1998@hotmail.com>
2019-05-15 00:34:10 -0300

    Art and Dialogue Test | Fix empty button option | Build

---
+++ Challenge_Game/Assets/Scripts/PointManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PointManager : MonoBehaviour
+{
+    public float defaultPoints;
+    public float curPoints;
+    public float requiredPoints;
+
+    public Text pointsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curPoints = defaultPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // PLACEHOLDER 
+        pointsText.text = "Pontuação: " + curPoints;
+    }
+
+    public void AddPoints(int value)
+    {
+        curPoints += value;
+    }
+}
